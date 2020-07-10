4faf72f33c6f899f3c063b23a855eeac854d180f
Armando Zubillaga <ajzpiv97@gmail.com>
2020-07-10 18:25:53 -0400

    Fixed Phone Validation Method

--- forms.py
+++ forms.py
@@ -204,9 +204,8 @@ class ArtistForm(FlaskForm):
     @staticmethod
     def validate_phone(phone):
         regex = "\w{3}\w{3}\w{4}"
-        match = re.search(regex, phone)
-        if not match:
-            return False
+        if re.search(regex, phone):
+            return True
 
     image_link = StringField(
         'image_link'
